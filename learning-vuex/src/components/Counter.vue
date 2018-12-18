e8ef380588b82c64c1d52bee690337477decbb2e
Andrew Shengilevych <ashengilevych@n-ix.com>
2018-12-18 19:08:36 +0200

    Vuex - documentation - first steps

---
+++ learning-vuex/src/components/Counter.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <h1>Counter</h1>
+        <div> {{ count }} </div>
+        <p>
+            <button @click="increment">+</button>
+            <button @click="decrement">-</button>
+        </p>
+    </div>
+</template>
+
+<script>
+export default 
+{
+  name: 'Counter',
+  computed: {
+      count () {
+          return this.$store.state.count
+      }
+  },
+  methods: {
+    increment () {
+        this.$store.commit('increment')
+    },
+    decrement () {
+        this.$store.commit('decrement')
+    }
+  }
+}
+</script>
